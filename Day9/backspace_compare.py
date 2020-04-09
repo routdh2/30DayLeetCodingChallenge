1a2f426df456b0ac3d197707dedf10a94b0b9362
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-09 16:04:37 +0530

    backspace compare program in python

---
+++ Day9/backspace_compare.py
@@ -0,0 +1,21 @@
+'''Problem Statement: https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3291/'''
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        r1=[]
+        r2=[]
+        for char in S:
+            if char=="#":
+                if len(r1)!=0:
+                    r1.pop()
+            else:
+                r1.append(char)
+        print(r1)
+        for char in T:
+            if char=="#":
+                if len(r2)!=0:
+                    r2.pop()
+            else:
+                r2.append(char)
+        print(r2)
+        return r1==r2
+        
