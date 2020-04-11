497af05875cbfb41e942f004bb4fd8671373e4da
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-11 15:58:49 +0530

    diameter of binary tree code in python

---
+++ Day_11/diameter_of_binary_tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+'''Problem Statement: https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3293/'''
+
+class Solution:
+    diameter=0
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        if root==None:
+            return 0
+        l_height=self.find_height(root.left)
+        r_height=self.find_height(root.right)
+        total=l_height+r_height
+        if total > self.diameter:
+            self.diameter=total
+        return self.diameter
+    
+    def find_height(self,root):
+        if root==None:
+            return 0
+        if root.left==None and root.right==None:
+            return 1
+        l_height=self.find_height(root.left)
+        r_height=self.find_height(root.right)
+        total=l_height+r_height
+        if total>self.diameter:
+            self.diameter=total
+        return 1+max(l_height,r_height)
+        
+        
